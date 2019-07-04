ef5dfb392dd02df5cdffdeeefa7de9c76c342318
liVyO9 <kubasitnik79@gmail.com>
2019-07-04 15:27:30 +0200

    changes on CMS admin in about-people-page

--- src/pages/about-people/index.js
+++ src/pages/about-people/index.js
@@ -11,8 +11,7 @@ export default class Index extends React.Component {
   render() {
     return (
         <Layout>
-            <div>
-            </div>
+            
         </Layout>
     )
   }
