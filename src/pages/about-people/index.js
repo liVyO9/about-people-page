d0d10f7f7beaf29b28df443e542c788646e3e457
kubasitnik79 <kubasitnik79@gmail.com>
2019-07-08 09:29:08 +0000

    Update Pages “people”

--- src/pages/about-people/index.js
+++ src/pages/about-people/index.js
@@ -1,18 +1,1 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../../components/Layout'
-
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    return (
-        <Layout>
-            
-        </Layout>
-    )
-  }
-}
+undefined
