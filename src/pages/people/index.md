38e91ec68e1ac88b9fdae87f575b30f595fe3893
liVyO9 <kubasitnik79@gmail.com>
2019-07-11 15:30:14 +0200

    new brand /people page

--- src/pages/people/index.md
+++ src/pages/people/index.md
@@ -1,7 +1,10 @@
----
-templateKey: 'people-page'
+--- 
+info: 
+  description1: "Gwarancja dynamicznego rozwoju, nowe technologie, innowacyjne podejście. Tym wszystkim szczyci się większość firm w branży IT. Specjalistyczna wiedza i doświadczenie to także norma, którą obiecuje każdy. My również, ale oprócz tego mamy do zaoferowania coś więcej – zgrany zespół najlepszych specjalistów, którzy gotowi są pomóc Ci rozwiązać każdy technologiczny problem.\n"
+  description2: "AKRA Polska powstała w styczniu 2012 roku w Krakowie jako siostrzana firma prestiżowej niemieckiej AKRA GmbH z siedzibą w Hamburgu, działającej na rynku IT od 1999 roku. Stale doskonalimy się w zakresie najnowszych technologii, by świadczyć usługi najwyższej jakości. Zdobytym doświadczeniem chętnie dzielimy się z naszymi klientami, wskazując im drogę do sukcesu. Pomagamy również sobie nawzajem, bowiem wychodzimy z założenia, że silny, wspierający się zespół jest w stanie zdziałać o wiele więcej.\n"
+  description3: "Wierzymy, że nasza pozytywna energia oraz gotowość na nowe wyzwania to ogromny atut w dynamicznym świecie IT. Nasi klienci wiedzą, że mogą na nas polegać.\n"
+  image: /img/europe-map.png
 path: /people
-title: O NAS
-image: /img/europe-map.png
-description: 
----
+team: Zdzisiek
+templateKey: people-page
+title: "O NAS"
--- src/templates/people-page.js
+++ src/templates/people-page.js
@@ -6,22 +6,26 @@ import Img from 'gatsby-image'
 
 export const PeoplePageTemplate = ({
   title,
-  description,
-  image,
+  info,
 }) =>{
     return(
-        <div className="content">
-            <div className="">
-                <div>
+        <div className="container">
+            <div className="aboutAKRA-container">
+                <div className="aboutAKRA-container-title">
                     {title}
                 </div>
+                <div className="aboutAKRA-container-description">
+                    <div className="aboutAKRA-container text-part">
+                        <div>{info.description1}</div>
+                        <div>{info.description2}</div>
+                        <div>{info.description3}</div>
+                    </div>
+                    <div className="aboutAKRA-container img-part">
+                        {info.image.childImageSharp ? <Img fluid={{...info.image.childImageSharp.fluid, sizes: '(max-width: 512px) 20vw, (max-width: 768px) 35vw, 430px'}} alt='' /> : ""
+}                    </div>
+                </div>
                 <div>
-                    <div className="">
-                        {description}
-                    </div>
-                    <div className="">
-                        <Img fluid={image.childImageSharp.fluid} alt='' />
-                    </div>
+
                 </div>
             </div>
         </div>
@@ -29,8 +33,8 @@ export const PeoplePageTemplate = ({
 }
 
 PeoplePageTemplate.propTypes = {
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     title: PropTypes.string.isRequired,
+    info: PropTypes.object,
 }
 
 const PeoplePage = ({data}) =>{
@@ -39,9 +43,8 @@ const PeoplePage = ({data}) =>{
     return(
         <Layout>
             <PeoplePageTemplate
-            image={frontmatter.image}
             title={frontmatter.title}
-            description={frontmatter.description}
+            info={frontmatter.info}
             />
         </Layout>
     )
@@ -62,15 +65,18 @@ query PeoplePage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
-        title
-        image {
-            childImageSharp {
-              fluid(maxWidth: 600,maxHeight: 600, quality: 100) {
-                ...GatsbyImageSharpFluid
+        info{
+            image {
+                childImageSharp {
+                  fluid(maxWidth: 600, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
               }
-            }
-          }
-        description
+            description1
+            description2
+            description3
+        }
       }
     }
   }
